ff901fcc9d705b95b0838ecc4c66e7d711fc1ece
Hejinzefinance <Hejinze_finance@pku.edu.cn>
2021-03-15 21:23:28 +0800

    code version-2: code reconstruction

--- main_code.py
+++ main_code.py
@@ -4,8 +4,6 @@ Created on Mon Mar 15 20:53:18 2021
 
 @author: Hejinze
 """
-
+from my_functions import add
 print('Hello World')
-def add(x,y):
-    return x+y
 print(add(1,3))
---
+++ my_functions.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 15 21:21:17 2021
+
+@author: Hejizne
+"""
+def add(x,y):
+    return x+y
